dbcf28c0cd10672f8375caeb09eec7a538d2c4aa
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2023-11-22 14:41:11 +0900

    u

--- docs/TVonline/script_tv.js
+++ docs/TVonline/script_tv.js
@@ -1,66 +1,4 @@
-// These are functions to pass to html file
-function loadVideo(videoUrl, new_agent = '') {
-    set_userAgent(new_agent) // set user-agent
-    // if (Array.isArray(videoUrls)) {
-    //     var videoUrl = videoUrls[0]
-    // } else {
-    //     var videoUrl = videoUrls
-    // }
-    window.scrollTo(0, 0); // Scroll to the top after loading the video
-    var player = videojs('vid1');
-    // Call plugin here, before load src
-    // player.hlsQualitySelector({displayCurrentQuality: true});
-    player.src({ src: videoUrl, type: 'application/x-mpegURL' });
-    player.play();
-};
 
-
-// Automatically load and play default video when page loads
-window.addEventListener('load', function () {
-    loadVideo('https://ctrl.laotv.la/live/HBO/index.m3u8');
-});
-
-
-function loadStream(new_agent = '') {
-    set_userAgent(new_agent) // set user-agent
-    var videoUrl = document.getElementById("m3u8Link").value;
-    if (!videoUrl) {
-        alert("Please enter a stream link.");
-        return;
-    };
-    window.scrollTo(0, 0);
-    var player = videojs('vid1');
-    player.src({ src: videoUrl, type: 'application/x-mpegURL' });
-    player.play();
-};
-
-
-function loadYoutube(videoUrl) {
-    window.scrollTo(0, 0);
-    var player = videojs('vid1', {
-        "techOrder": ["youtube"], // Use YouTube as the primary playback technology
-        "sources": [{ "type": "video/youtube", "src": videoUrl }]
-    });
-    player.play();
-}
-
-function loadHLS(videoUrl) {    // or name as: loadHLS
-    window.scrollTo(0, 0);
-    var video = document.getElementById('vid1');
-    if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(videoUrl);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
-            video.play();
-        });
-    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-        video.src = videoUrl;
-        video.addEventListener('canplay', function () {
-            video.play();
-        });
-    }
-}
 
 
 
@@ -146,4 +84,76 @@ function set_userAgent(new_agent) {
     agent += new_agent;
     makePropertyWritable(window, "navigator", "userAgent");
     window.navigator.userAgent = agent;
+}
+
+
+
+
+
+
+
+
+
+// These are functions to pass to html file
+function loadVideo(videoUrl, new_agent = '') {
+    set_userAgent(new_agent) // set user-agent
+    // if (Array.isArray(videoUrls)) {
+    //     var videoUrl = videoUrls[0]
+    // } else {
+    //     var videoUrl = videoUrls
+    // }
+    window.scrollTo(0, 0); // Scroll to the top after loading the video
+    var player = videojs('vid1');
+    // Call plugin here, before load src
+    // player.hlsQualitySelector({displayCurrentQuality: true});
+    player.src({ src: videoUrl, type: 'application/x-mpegURL' });
+    player.play();
+};
+
+
+// Automatically load and play default video when page loads
+window.addEventListener('load', function () {
+    loadVideo('https://ctrl.laotv.la/live/HBO/index.m3u8');
+});
+
+
+function loadStream(new_agent = '') {
+    set_userAgent(new_agent) // set user-agent
+    var videoUrl = document.getElementById("m3u8Link").value;
+    if (!videoUrl) {
+        alert("Please enter a stream link.");
+        return;
+    };
+    window.scrollTo(0, 0);
+    var player = videojs('vid1');
+    player.src({ src: videoUrl, type: 'application/x-mpegURL' });
+    player.play();
+};
+
+
+function loadYoutube(videoUrl) {
+    window.scrollTo(0, 0);
+    var player = videojs('vid1', {
+        "techOrder": ["youtube"], // Use YouTube as the primary playback technology
+        "sources": [{ "type": "video/youtube", "src": videoUrl }]
+    });
+    player.play();
+}
+
+function loadHLS(videoUrl) {    // or name as: loadHLS
+    window.scrollTo(0, 0);
+    var video = document.getElementById('vid1');
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoUrl);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = videoUrl;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+    }
 }
