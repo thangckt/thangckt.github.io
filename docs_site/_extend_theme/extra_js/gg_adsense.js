a36141abbeee99cea424b3de8645c3e0c0750b89
C. Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-04-08 17:13:59 +0900

    u

--- docs_site/_extend_theme/extra_js/gg_adsense.js
+++ docs_site/_extend_theme/extra_js/gg_adsense.js
@@ -1,2 +1,2 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6260920536615462"
-     crossorigin="anonymous"></script>
+{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6260920536615462"
+     crossorigin="anonymous"></script> */}
